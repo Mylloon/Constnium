dbe054e51b8af1139fb0fcd153eaa6e46a3193bf
Mylloon <kennel.anri@tutanota.com>
2022-06-23 13:00:41 +0200

    Add constants for a,b,c,e,f,g,k,l,m,p,s

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,7 @@ const main = () => {
 // Callback: called when firstname is changed
 const update_const = () => {
     const firstname = document.getElementById(input_name).value;
-    const firstname_const = get_const(firstname.split(''));
+    const firstname_const = get_const(firstname.toLowerCase().split(''));
 
     console.log(`output: ${firstname_const}`);
 };
@@ -29,44 +29,44 @@ const get_const = (letters = Array) => {
 
     // Assign each letter to a const, fallback to 1 if no one has been found
     switch (letters) {
-        /* case 'a':
-            const_data.push(); */
-        /* case 'b':
-            const_data.push(); */
-        /* case 'c':
-            const_data.push(); */
+        case 'a': // https://en.wikipedia.org/wiki/Artin%27s_conjecture_on_primitive_roots
+            const_data.push(0.373955);
+        case 'b': // https://en.wikipedia.org/wiki/Backhouse%27s_constant
+            const_data.push(1.456074);
+        case 'c': // https://en.wikipedia.org/wiki/Porter%27s_constant
+            const_data.push(1.46707807943397547289);
         /* case 'd':
             const_data.push(); */
-        /* case 'e':
-            const_data.push(); */
-        /* case 'f':
-            const_data.push(); */
-        /* case 'g':
-            const_data.push(); */
+        case 'e': // https://en.wikipedia.org/wiki/Erd%C5%91s%E2%80%93Borwein_constant
+            const_data.push(1.606695);
+        case 'f': // https://en.wikipedia.org/wiki/Frans%C3%A9n%E2%80%93Robinson_constant
+            const_data.push(2.807770);
+        case 'g': // https://en.wikipedia.org/wiki/Gauss%27s_constant
+            const_data.push(0.834626);
         /* case 'h':
             const_data.push(); */
         /* case 'i':
             const_data.push(); */
         /* case 'j':
             const_data.push(); */
-        /* case 'k':
-            const_data.push(); */
-        /* case 'l':
-            const_data.push(); */
-        /* case 'm':
-            const_data.push(); */
+        case 'k': // https://en.wikipedia.org/wiki/Landau%E2%80%93Ramanujan_constant
+            const_data.push(0.764223);
+        case 'l': // https://en.wikipedia.org/wiki/Lochs%27s_theorem
+            const_data.push(0.97027011439203392574);
+        case 'm': // https://en.wikipedia.org/wiki/Meissel%E2%80%93Mertens_constant
+            const_data.push(0.261497);
         /* case 'n':
             const_data.push(); */
         /* case 'o':
             const_data.push(); */
-        /* case 'p':
-            const_data.push(); */
+        case 'p': // https://en.wikipedia.org/wiki/Universal_parabolic_constant
+            const_data.push(2.29558714939263807403);
         /* case 'q':
             const_data.push(); */
         /* case 'r':
             const_data.push(); */
-        /* case 's':
-            const_data.push(); */
+        case 's': // https://en.wikipedia.org/wiki/MRB_constant
+            const_data.push(0.18785964246206712024);
         /* case 't':
             const_data.push(); */
         /* case 'u':
