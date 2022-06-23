f57565d7802e9736212a82831fdcf772a770ac5c
Mylloon <kennel.anri@tutanota.com>
2022-06-23 12:50:11 +0200

    Add top 10 firstname girls and boys worldwide

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,27 @@
 const randomFirstname = () => {
-    return "Maria";
+    const firstnames = [
+        "Sarah",
+        "Emma",
+        "Laura",
+        "Chloé",
+        "Marie",
+        "Emily",
+        "Léa",
+        "Camille",
+        "Anna",
+        "Manon",
+        "Alex",
+        "Thomas",
+        "David",
+        "Daniel",
+        "Kévin",
+        "Michael",
+        "Nicolas",
+        "James",
+        "John",
+        "Lucas"
+    ]
+    return firstnames[Math.floor(Math.random() * firstnames.length)];
 };
 
 module.exports = {
